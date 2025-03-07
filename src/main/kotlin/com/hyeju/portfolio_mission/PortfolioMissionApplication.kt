d3d22aff6514f23b736e072231c0fcad3ee93e22
simhyeju <simhyeju89@gmail.com>
2025-03-08 02:30:47 +0900

    [미션1] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/hyeju/portfolio_mission/PortfolioMissionApplication.kt
@@ -0,0 +1,11 @@
+package com.hyeju.portfolio_mission
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioMissionApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortfolioMissionApplication>(*args)
+}
---
+++ src/test/kotlin/com/hyeju/portfolio_mission/PortfolioMissionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyeju.portfolio_mission
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioMissionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
